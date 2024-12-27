901ac03cc2b066b4428e4681c517eaf06403693b
funZ <153361103+PaFunzFr@users.noreply.github.com>
2024-12-27 16:00:17 +0100

    Update 241127

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 let picContent = document.getElementById('picContent');
 let btnShowMore = document.getElementById('btnShowMore');
 let btnShowLess = document.getElementById('btnShowLess');
+
 picContainer = [];
 
+// initialize picture array (container)
 for (let i = 0; i < 26; i++) {
     picContainer.push(`./assets/img/03_pics/${i+1}.jpg`);
 }
 
+function createElement(id) {
+    picContent.innerHTML += 
+        `<img onclick="popOverlay(${id})" class="img-card" src="${picContainer[id]}">`
+    };
+
+
 
 function renderInit() {
     for (let i = 0; i < 12; i++) {
-        picContent.innerHTML += 
-        `<img class="img-card" src="${picContainer[i]}">`
-        }
-};
+        createElement(i);
+}};
 
 function renderAll() {
     picContent.innerHTML = '';
     btnShowMore.classList.add('hidden');
     btnShowLess.classList.remove('hidden');
     for (let i = 0; i < picContainer.length; i++) {
-        picContent.innerHTML += 
-        `<img class="img-card" src="${picContainer[i]}">`
+        createElement(i);
         };
 };
 
@@ -29,8 +34,25 @@ function renderLess() {
     picContent.innerHTML = '';
     btnShowMore.classList.remove('hidden');
     btnShowLess.classList.add('hidden');
-    for (let i = 0; i < 12; i++) {
-        picContent.innerHTML += 
-        `<img class="img-card" src="${picContainer[i]}">`
-        }
+    renderInit()
 };
+
+function popOverlay(id) {
+    let getOverlay = document.getElementById('overlay');
+    getOverlay.classList.remove('hidden');
+    getOverlay.classList.add('flex-shown');
+    getOverlay.innerHTML += `
+        <div class="app-max-width overlay-container">
+            <div class="overlay-content">
+                <button class="btn-close-slider">X</button>
+                <ul class="slider">
+                    <li id="btn-previous"><</li>
+                    <li id="btn-forward">></li>
+                </ul>
+                <img src="./assets/img/03_pics/${id+1}.jpg">
+            </div>
+        </div>`
+}
+
+
+
---
+++ script_archive.js
@@ -0,0 +1,8 @@
+        /*
+    let imgElement = document.createElement('img'); // build <img> element
+    imgElement.classList.add(`img-card`);
+    imgElement.id = `img${id}`;
+    imgElement.src = picContainer[id];
+    picContent.appendChild(imgElement);
+    imgElement.addEventListener('click', popOverlay);
+}*/
